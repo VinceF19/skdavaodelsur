b37da97b1a745b7ad252dfb3ba732c84708afd7b
Vince Fernandez <Vince.fernandezg@gmail.com>
2024-12-14 12:02:09 +0800

    navbar

--- skdavaodelsur/src/App.jsx
+++ skdavaodelsur/src/App.jsx
@@ -1,15 +1,13 @@
-import Navbar from './components/Navbar'
-import './App.css'
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-    <h1>Hello World!</h1>
-    <Navbar/>
+      <Navbar />
+      <h1>Hello World!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- skdavaodelsur/src/components/Navbar.jsx
+++ skdavaodelsur/src/components/Navbar.jsx
@@ -1,37 +1,40 @@
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <div className="d-flex justify-content-centers">
+      
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Home</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Government</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">News & Events</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About us</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contact Us
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a className="dropdown-item" href="#">Contact</a></li>
+            <li><a className="dropdown-item" href="#">Survey</a></li>
           </ul>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+
+</div>
   )
 }
 
